79d4cd55067b37726eec6ce8939b0238905e5819
aren talb <aren.talb00@gmail.com>
2024-05-15 19:10:07 +0300

    Separate the usersPage main functionality to another component

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,3 +1,42 @@
+import { FiCheck, FiX } from "react-icons/fi";
+import { useGetUsersQuery } from "../redux/api/usersApiSlice.js";
+
 export function UserList() {
-  return <></>;
+  const { data, isLoading, error } = useGetUsersQuery();
+  const users = data ? data.data : [];
+
+  return (
+    <>
+      {isLoading ? (
+        <div className={"w-full flex justify-center items-center"}>
+          <span className="loading loading-spinner loading-md"></span>
+        </div>
+      ) : (
+        <div className="overflow-x-auto">
+          <table className="table">
+            <thead>
+              <tr>
+                <th></th>
+                <th>ID</th>
+                <th>USERNAME</th>
+                <th>EMAIL</th>
+                <th>IS ADMIN</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user, index) => (
+                <tr key={user._id}>
+                  <th>{index + 1}</th>
+                  <td>{user._id}</td>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                  <td>{user.isAdmin ? <FiCheck /> : <FiX />}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </>
+  );
 }
--- src/pages/admin/UsersPage.jsx
+++ src/pages/admin/UsersPage.jsx
@@ -1,43 +1,10 @@
-import { useGetUsersQuery } from "../../redux/api/usersApiSlice.js";
-import { FiCheck, FiX } from "react-icons/fi";
+import { UserList } from "../../components/UserList.jsx";
 
 export function UsersPage() {
-  const { data, isLoading, error } = useGetUsersQuery();
-  const users = data ? data.data : [];
-
   return (
     <div className={" py-10 "}>
       <h1 className={"text-3xl font-bold mb-8"}>All users </h1>
-      {isLoading ? (
-        <div className={"w-full flex justify-center items-center"}>
-          <span className="loading loading-spinner loading-md"></span>
-        </div>
-      ) : (
-        <div className="overflow-x-auto">
-          <table className="table">
-            <thead>
-              <tr>
-                <th></th>
-                <th>ID</th>
-                <th>USERNAME</th>
-                <th>EMAIL</th>
-                <th>IS ADMIN</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user, index) => (
-                <tr key={user._id}>
-                  <th>{index + 1}</th>
-                  <td>{user._id}</td>
-                  <td>{user.username}</td>
-                  <td>{user.email}</td>
-                  <td>{user.isAdmin ? <FiCheck /> : <FiX />}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      )}
+      <UserList />
     </div>
   );
 }
